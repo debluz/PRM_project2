d0d806aa4c38c87a45f1e70a279ff4c741ce766b
DeBluzNDT <67010522+debluz@users.noreply.github.com>
2020-06-20 18:20:58 +0200

    facebook login part 2

---
+++ app/src/main/java/com/example/prm_project2/LocationsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prm_project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_locations)
+    }
+}
